7b2c7c4aad7d8b3f6a975ac8c3071b8ff93af8b8
Gregory Coomber <gr.coomber@gmail.com>
2025-04-12 18:07:56 -0700

    Added the comparison and equality bytecode, and runtime behaviour.

--- c-lox/src/compiler.c
+++ c-lox/src/compiler.c
@@ -194,10 +194,16 @@ static void binary(void) {
     parse_precedence((precedence)(rule->prec + 1));
 
     switch (operator_type) {
-        case TOKEN_PLUS:  emit_byte(OP_ADD);      break;
-        case TOKEN_MINUS: emit_byte(OP_SUBTRACT); break;
-        case TOKEN_STAR:  emit_byte(OP_MULTIPLY); break;
-        case TOKEN_SLASH: emit_byte(OP_DIVIDE);   break;
+        case TOKEN_BANG_EQUAL:    emit_bytes2(OP_EQUAL, OP_NOT);   break;
+        case TOKEN_EQUAL_EQUAL:   emit_byte(OP_EQUAL);             break;
+        case TOKEN_GREATER:       emit_byte(OP_GREATER);           break;
+        case TOKEN_GREATER_EQUAL: emit_bytes2(OP_LESS, OP_NOT);    break;
+        case TOKEN_LESS:          emit_byte(OP_LESS);              break;
+        case TOKEN_LESS_EQUAL:    emit_bytes2(OP_GREATER, OP_NOT); break;
+        case TOKEN_PLUS:          emit_byte(OP_ADD);               break;
+        case TOKEN_MINUS:         emit_byte(OP_SUBTRACT);          break;
+        case TOKEN_STAR:          emit_byte(OP_MULTIPLY);          break;
+        case TOKEN_SLASH:         emit_byte(OP_DIVIDE);            break;
         default: return;
     }
 }
--- c-lox/src/value.c
+++ c-lox/src/value.c
@@ -35,3 +35,16 @@ void print_value(value val) {
         } break;
     }
 }
+
+bool values_equal(value a, value b) {
+    if (a.type != b.type) {
+        return false;
+    }
+
+    switch (a.type) {
+        case VAL_BOOL:   return AS_BOOL(a) == AS_BOOL(b);
+        case VAL_NULL:   return true;
+        case VAL_NUMBER: return AS_NUMBER(a) == AS_NUMBER(b);
+        default: return false;
+    }
+}
