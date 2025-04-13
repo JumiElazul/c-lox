4f7abc866947cbe6821c97fdfd14de9ea0929f28
Gregory Coomber <gr.coomber@gmail.com>
2025-04-13 11:40:50 -0700

    Added object file, with our internal string representation obj_string

---
+++ c-lox/src/object.h
@@ -0,0 +1,32 @@
+#ifndef JUMI_CLOX_OBJECT_H
+#define JUMI_CLOX_OBJECT_H
+#include "common.h"
+#include "value.h"
+
+typedef enum {
+    OBJ_STRING,
+} obj_type;
+
+struct obj {
+    obj_type type;
+};
+
+struct obj_string {
+    obj obj;
+    int length;
+    char* chars;
+};
+
+static inline bool is_obj_type(value val, obj_type type) {
+    return IS_OBJ(val) && AS_OBJ(val)->type == type;
+}
+
+#define OBJ_TYPE(val)   (AS_OBJ(value)->type)
+
+#define IS_STRING(val)  is_obj_type(val, OBJ_STRING)
+
+#define AS_STRING(val)  ((obj_string*)AS_OBJ(val))
+
+#define AS_CSTRING(val) ((obj_string*)AS_OBJ(val)->chars)
+
+#endif
--- c-lox/src/value.h
+++ c-lox/src/value.h
@@ -2,10 +2,14 @@
 #define JUMI_CLOX_VALUE_H
 #include "common.h"
 
+typedef struct obj obj;
+typedef struct obj_string obj_string;
+
 typedef enum value_type {
     VAL_BOOL,
     VAL_NULL,
     VAL_NUMBER,
+    VAL_OBJ,
 } value_type;
 
 typedef struct value {
@@ -13,20 +17,24 @@ typedef struct value {
     union {
         bool boolean;
         double number;
+        obj* obj;
     } as;
 } value;
 
 // Promotes C values to our internal c-lox value types
-#define BOOL_VAL(val)   ((value){ VAL_BOOL,   { .boolean = val } })
-#define NULL_VAL        ((value){ VAL_NULL,   { .number = 0    } })
-#define NUMBER_VAL(val) ((value){ VAL_NUMBER, { .number = val  } })
+#define BOOL_VAL(val)   ((value){ VAL_BOOL,   { .boolean = val   } })
+#define NULL_VAL        ((value){ VAL_NULL,   { .number = 0      } })
+#define NUMBER_VAL(val) ((value){ VAL_NUMBER, { .number = val    } })
+#define OBJ_VAL(val)    ((value){ VAL_OBJ,    { .obj = (obj*)val } })
 
 #define AS_BOOL(val)    ((val).as.boolean)
 #define AS_NUMBER(val)  ((val).as.number)
+#define AS_OBJ(val)     ((val).as.obj)
 
 #define IS_BOOL(val)    ((val).type == VAL_BOOL)
 #define IS_NULL(val)    ((val).type == VAL_NULL)
 #define IS_NUMBER(val)  ((val).type == VAL_NUMBER)
+#define IS_OBJ(val)     ((val).type == VAL_OBJ)
 
 typedef struct value_array {
     int capacity;
