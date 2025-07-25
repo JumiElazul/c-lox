c36828d5d575c299baeca3340ddfde8b76040bca
Gregory Coomber <gr.coomber@gmail.com>
2025-07-25 16:45:23 -0700

    Added binary instructions and stack manipulation to opcodes [CHAPTER 15 DONE]

--- c-lox/src/main.c
+++ c-lox/src/main.c
@@ -10,10 +10,16 @@ int main(int argc, const char* argv[]) {
 
     write_constant(&chunk, 1.2, 1);
     write_constant(&chunk, 3.9, 2);
-    write_constant(&chunk, 0.5, 2);
-    write_constant(&chunk, 25, 2);
-    write_constant(&chunk, 420, 3);
-    write_to_bytecode_chunk(&chunk, OP_RETURN, 4);
+    write_to_bytecode_chunk(&chunk, OP_ADD, 2);
+    write_constant(&chunk, 0.9, 3);
+    write_to_bytecode_chunk(&chunk, OP_ADD, 2);
+    write_to_bytecode_chunk(&chunk, OP_NEGATE, 3);
+    write_to_bytecode_chunk(&chunk, OP_NEGATE, 3);
+    write_constant(&chunk, 3, 3);
+    write_to_bytecode_chunk(&chunk, OP_MULTIPLY, 4);
+    write_constant(&chunk, 2, 4);
+    write_to_bytecode_chunk(&chunk, OP_DIVIDE, 4);
+    write_to_bytecode_chunk(&chunk, OP_RETURN, 5);
 
     disassemble_chunk(&chunk, "test chunk");
 
